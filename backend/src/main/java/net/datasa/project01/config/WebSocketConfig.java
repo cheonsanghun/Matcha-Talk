d4441094deba3052b8058fe68568cd015529884e
cheonsanghun <mulchil123@naver.com>
2025-09-21 16:10:56 +0900

    update

--- backend/src/main/java/net/datasa/project01/config/WebSocketConfig.java
+++ backend/src/main/java/net/datasa/project01/config/WebSocketConfig.java
@@ -27,7 +27,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-stomp")
-                .setAllowedOriginPatterns("http://localhost:5173") // 개발용
+                .setAllowedOriginPatterns("http://localhost:5173","https://*.ngrok-free.app") // 개발용
                 .withSockJS();  // ★ SockJS 필수
     }
 
