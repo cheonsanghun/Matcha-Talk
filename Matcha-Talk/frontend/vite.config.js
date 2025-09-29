f02e1ebb6c761a512a24005ccd592fab78a78093
cheonsanghun <mulchil123@naver.com>
2025-09-29 13:39:35 +0900

    update

--- Matcha-Talk/frontend/vite.config.js
+++ Matcha-Talk/frontend/vite.config.js
@@ -4,12 +4,16 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
+    https: {
+      cert: fs.readFileSync(new URL('./certs/dev-cert.pem', import.meta.url)),
+      key: fs.readFileSync(new URL('./certs/dev-key.pem', import.meta.url)),
+    },
     port: 5173,
     host: '0.0.0.0', // 외부 접근 허용
     open: true,
     allowedHosts: [
       '.ngrok-free.app', // ngrok 도메인 허용
-      '192.168.0.126',   // ✅ 추가: 서버 IP 허용
+      '192.168.0.165',   // ✅ 추가: 서버 IP 허용
       '192.168.*',        // ✅ 추가: 같은 네트워크 대역 허용
       //'192.0.0.2',       // ✅ 추가: 핫스팟 서버 IP 허용
       //'192.0.0.*'        // ✅ 추가: 같은 핫스팟 네트워크 대역 허용
@@ -17,7 +21,7 @@ export default defineConfig({
     proxy: {
       // /api로 시작하는 요청을 백엔드로 프록시
       '/api': {
-        target: 'http://192.168.0.126:8080', // ✅ 수정: 실제 서버 IP로 변경
+        target: 'https://192.168.0.126:8080', // ✅ 수정: 실제 서버 IP로 변경
         //target: 'http://192.0.0.2:8080', // ✅ 수정: 실제 핫스팟 서버 IP로 변경
         changeOrigin: true,
         // 필요하다면 secure: false 추가
